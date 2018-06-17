db3cf13e5cdba2359896cfbef9d0cab9a41ad9d5
Matt R <therieldesign@gmail.com>
2018-06-17 22:34:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ function destructivelyPrependKitten(name) {
   return kittens;
 }
 
-function destructivelyPrependKitten(name) {
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(name) {
   kittens.shift(name);
   return kittens;
 }
+
